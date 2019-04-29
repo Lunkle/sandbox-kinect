622b4a5cf9d3ae45f611796f5d38a34474754a9f
Lunkle <bobbob.donny@gmail.com>
2019-04-28 21:43:28 -0400

    sort of working??

--- src/kinect/Kinect.java
+++ src/kinect/Kinect.java
@@ -4,14 +4,31 @@ import edu.ufl.digitalworlds.j4k.J4KSDK;
 
 public class Kinect extends J4KSDK {
 
-	@Override
-	public void onColorFrameEvent(byte[] arg0) {
-		System.out.println("Color frame event");
+	public float[][] depthMap;
+	public boolean depthMapLoaded = false;
+
+	public Kinect() {
+		depthMap = new float[424][512];
 	}
 
 	@Override
-	public void onDepthFrameEvent(short[] arg0, byte[] arg1, float[] arg2, float[] arg3) {
+	public void onColorFrameEvent(byte[] arg0) {
+		System.out.println("Color frame event");
+
+	}
+
+	@Override
+	public void onDepthFrameEvent(short[] depth_frame, byte[] player_index, float[] XYZ, float[] UV) {
 		System.out.println("Depth frame event");
+		depthMapLoaded = true;
+		int index = 0;
+		for (int y = 0; y < getDepthHeight(); y++) {
+			for (int x = 0; x < getDepthWidth(); x++) {
+				float value = XYZ[index];
+				depthMap[y][x] = value;
+				index++;
+			}
+		}
 	}
 
 	@Override
--- src/main/SandboxApp.java
+++ src/main/SandboxApp.java
@@ -2,17 +2,51 @@ package main;
 
 import edu.ufl.digitalworlds.j4k.J4KSDK;
 import kinect.Kinect;
+import processing.core.PApplet;
 
-public class SandboxApp {
+public class SandboxApp extends PApplet {
+
+	Kinect kinect;
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		Kinect kinect = new Kinect();
-		kinect.start(J4KSDK.COLOR | J4KSDK.DEPTH | J4KSDK.SKELETON);
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
+		PApplet.main("main.SandboxApp");
+	}
+
+	public void settings() {
+		size(512, 424);
+	}
+
+	public void setup() {
+		kinect = new Kinect();
+		kinect.start(J4KSDK.DEPTH | J4KSDK.XYZ);
+		fill(120, 50, 240);
+	}
+
+	public void draw() {
+		if (kinect.depthMapLoaded) {
+			loadPixels();
+			int index = 0;
+			for (int y = 0; y < 424; y++) {
+				for (int x = 0; x < 512; x++) {
+					pixels[index] = color((int) map(kinect.depthMap[y][x], 0, 5, 0, 255));
+//				System.out.println(kinect.dataMap[y][x]);
+					index++;
+				}
+			}
+			for (int x = 0; x < 2 * 512; x++) {
+				pixels[x] = color(255, 0, 0);
+			}
+			updatePixels();
 		}
+	}
+
+	public void mousePressed() {
+		close();
+	}
+
+	private void close() {
+		kinect.stop();
+		exit();
 	}
 
 }
